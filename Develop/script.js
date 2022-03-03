4bb2840a9987d0f5333d9a34a8fad51e0e66a07c
Equinox <cameron.minchin@gmail.com>
2022-03-03 19:47:55 +0800

    password generates based on charlist made from chars selected

--- Develop/script.js
+++ Develop/script.js
@@ -4,25 +4,26 @@ var passwordBox = document.querySelector("#password").parentNode;
 var slider = document.getElementById("lengthS");
 var sliderDisplay = document.getElementById("lengthP");
 var chars = document.getElementById("chars");
+var warning = document.getElementById("warning");
 
 
 // Write password to the #password input
 function writePassword() {
-  var charlist = ""
+  var charlist = "";
   Array.from(chars.children).forEach(char => {
-    console.log((char.children[0].checked));
     if (char.children[0].checked) {
-      charlist += char.getAttribute("-data");
+      charlist += char.children[0].getAttribute("data-chars");
     }
   });
 
   if (charlist.length) {
+    warning.setAttribute("style","display: none;");
     var password = generatePassword(charlist);
     var passwordText = passwordBox.children[0];
 
     passwordText.value = password;
   } else {
-    console.assert("select a box");
+    warning.setAttribute("style","color: red;");
   }
 
 }
@@ -31,9 +32,13 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 function generatePassword(charlist) {
+  var password = "";
+  c = charlist.split("");
+  for (var x = 0; x<slider.value; x++) {
+    password += c[Math.floor(Math.random()*charlist.length)];
+  }
 
-
-  return "baba yaga"
+  return password;
 }
 
 slider.oninput = function() {
